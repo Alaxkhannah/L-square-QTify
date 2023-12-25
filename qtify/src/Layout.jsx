507830e03fcb9bf0e506e5493e4070ff1ac92676
Alaxkhannah <143799373+Alaxkhannah@users.noreply.github.com>
2023-12-25 16:50:37 +0530

    Add files via upload

---
+++ qtify/src/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import HeroSection from "./Components/HeroSection/HeroSection";
+import "./Layout.css";
+
+const Layout = () => {
+    return (
+      <div className="layout">
+        <NavBar />
+        <HeroSection />
+        </div>
+        );
+        }
+
+export default Layout;
